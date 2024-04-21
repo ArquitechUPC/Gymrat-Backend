fc9ffb26f7d7857309eaea477add6962991b52ce
RamiroVT <u202123714@upc.edu.pe>
2024-04-21 11:13:37 -0500

    add: initial files

---
+++ src/main/java/org/Arquitech/Gymrat/GymratApplication.java
@@ -0,0 +1,13 @@
+package org.Arquitech.Gymrat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymratApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymratApplication.class, args);
+	}
+
+}
